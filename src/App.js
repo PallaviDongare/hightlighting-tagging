3e1240b3d796a475441bc2a536b11adbbecde863
PallaviDongare <pallavi.dongare@springernature.com>
2020-06-19 11:21:25 +0530

    Highlighting of words

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './App.css';
+import HighLighter from './Highlighter';
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <header className="App-header"> */}
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+
+      <h1>Highlighing </h1>
+      <HighLighter/>
+      {/* </header> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/drugs.js
@@ -0,0 +1,17 @@
+
+ const drug = [{
+	type: "drug",
+	term: "Trimethoprim sulfamethoxazole"
+},{
+	type: "drug",
+	term: "sulphonamide"
+},{
+	type: "disease",
+	term: "maculopapular exanthema (MPE)"
+},{
+	type: "disease",
+	term: "autoimmune-condition"
+}]
+
+
+export default drug;
